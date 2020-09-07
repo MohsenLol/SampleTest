69e1153379f39685e808a1be6c58af3a9c959f47
MohsenLol <freedragonstarm@gmail.com>
2020-09-07 14:44:43 +0430

    first revision

---
+++ lib.cpp
@@ -0,0 +1,33 @@
+/*******************************************************************************************
+ * File: lib.cpp
+ * 
+ * Copyright 2020 MohsenLol
+ * All Rights Reserved.
+ * 
+ * The information in this file is just for learning on My own!
+ * 
+ * Author: MohsenLol
+ * Date: Created September 2020
+ * 
+ * Description: A simple program for testing repositories 
+ *              only add and multiply two integer
+ * 
+ * Note: Just for FuN!!
+**********************************************************************************************/
+
+
+/*********************************************************************************************
+ *
+ * includes
+ * 
+ *********************************************************************************************/
+#include <iostream>
+#include "lib.h"
+using namespace std;
+ 
+int add(int a, int b){
+    return a+b;
+}
+int mult(int a, int b){
+    return a*b;
+}
---
+++ lib.h
@@ -0,0 +1,53 @@
+/*******************************************************************************************
+ * File: lib.h
+ * 
+ * Copyright 2020 MohsenLol
+ * All Rights Reserved.
+ * 
+ * The information in this file is just for learning on My own!
+ * 
+ * Author: MohsenLol
+ * Date: Created September 2020
+ * 
+ * Description: A simple program for testing repositories 
+ *              only add and multiply two integer
+ * 
+ * Note: Just for FuN!!
+**********************************************************************************************/
+
+
+/*********************************************************************************************
+ *
+ * includes
+ * 
+ *********************************************************************************************/
+#include <iostream>
+
+
+/*********************************************************************************************
+ * Function: mult
+ * Description: multiply two integer number 
+ * Parameters: 
+ *      int a
+ *      int b
+ * Return : 
+ *      (int) a*b
+ * 
+ *********************************************************************************************/
+int mult(int, int);
+
+
+/*********************************************************************************************
+ * Function: add
+ * Description: Add two integer number 
+ * Parameters: 
+ *      int a
+ *      int b
+ * Return : 
+ *      (int) a*b
+ * 
+ *********************************************************************************************/
+int add(int, int);
+
+
+
